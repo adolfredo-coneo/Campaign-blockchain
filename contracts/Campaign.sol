52b8080aeeca510d67075831f5699d68f0263f4a
KOBEsphinx242 <ado.coneo@gmail.com>
2021-09-11 17:20:09 -0500

    feat(factory): Add New CampaignFactory Contract to Delegate the Creation of New Campaigns

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -2,73 +2,91 @@
 
 pragma solidity >=0.5.0 <0.9.0;
 
-/**
- * @title Ballot
- * @dev Implements voting process along with vote delegation
+/** 
+ * @title CompaignFactory
+ * @dev Implements Campaign Factory for campaign creation
+ */
+contract CompaignFactory {
+    address[] public deployedCampaigns;
+    
+    function createCampaign(uint minimum) public {
+        address newCampaign =address(new Compaign(minimum, msg.sender));
+        
+        deployedCampaigns.push(newCampaign);
+    }
+    
+    function getDeployedCampaigns() public view returns (address[] memory) {
+        return deployedCampaigns;
+    }
+}
+
+/** 
+ * @title Compaign
+ * @dev Implements kickstarter like process along with campaign creation
  */
 contract Compaign {
     struct Request {
-        string description;
-        uint256 value;
-        address payable recipient;
-        bool complete;
-        uint256 approvalCount;
-        mapping(address => bool) approvals;
+      string description;
+      uint value;
+      address payable recipient;
+      bool complete;
+      uint approvalCount;
+      mapping(address => bool) approvals;
     }
-
+    
     address public manager;
-    mapping(uint256 => Request) public requests;
+    mapping(uint => Request) public requests;
     mapping(address => bool) approvers;
-    uint256 public approversCount;
-    uint256 public minimumContribution;
-    uint256 public numRequests;
-
-    modifier restricted() {
+    uint public approversCount;
+    uint public minimumContribution;
+    uint public numRequests;
+    
+    
+    
+   modifier restricted() {
         require(manager == msg.sender);
         _;
-    }
+   }    
 
-    constructor(uint256 minimum) {
-        manager = msg.sender;
+   constructor(uint minimum, address creator) {
+        manager = creator;
         minimumContribution = minimum;
-    }
-
-    function contribute() public payable {
+   }
+   
+   function contribute() public payable {
         require(msg.value >= minimumContribution);
         approvers[msg.sender] = true;
         approversCount++;
-    }
-
-    function createRequest(
-        string memory description,
-        uint256 value,
-        address payable recipient
-    ) public restricted {
-        Request storage newReq = requests[numRequests++];
-        newReq.description = description;
-        newReq.value = value;
-        newReq.recipient = recipient;
-        newReq.complete = false;
-        newReq.approvalCount = 0;
-    }
-
-    function approveRequest(uint256 index) public {
-        Request storage request = requests[index];
-
-        require(approvers[msg.sender]);
-        require(!request.approvals[msg.sender]);
-
-        request.approvals[msg.sender] = true;
-        request.approvalCount++;
-    }
-
-    function finalizeRequest(uint256 index) public restricted {
-        Request storage request = requests[index];
-
-        require(!request.complete);
-        require(request.approvalCount > (approversCount / 2));
-
-        request.recipient.transfer(request.value);
-        request.complete = true;
-    }
+   }
+   
+   function createRequest(string memory description, uint value, address payable recipient) public restricted {
+       Request storage newReq = requests[numRequests++];
+       newReq.description = description;
+       newReq.value = value;
+       newReq.recipient = recipient;
+       newReq.complete = false;
+       newReq.approvalCount = 0;
+   }
+   
+   function approveRequest(uint index) public {
+      Request storage request = requests[index];
+      
+      require(approvers[msg.sender]);
+      require(!request.approvals[msg.sender]);
+      
+      request.approvals[msg.sender] = true;
+      request.approvalCount++;
+   }
+   
+   function finalizeRequest(uint index) public restricted {
+      Request storage request = requests[index];
+      
+      require(!request.complete);
+      require(request.approvalCount > (approversCount / 2));
+      
+      request.recipient.transfer(request.value);
+      request.complete = true;
+   }
+    
+    
 }
