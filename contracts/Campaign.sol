383893860da1a50206508e8b960f670b4114874d
KOBEsphinx242 <ado.coneo@gmail.com>
2021-09-10 21:33:00 -0500

    feat(contract): Add newRequest push to Requests inside createRequest function

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -2,45 +2,54 @@
 
 pragma solidity >=0.5.0 <0.9.0;
 
-/** 
+/**
  * @title Ballot
  * @dev Implements voting process along with vote delegation
  */
 contract Compaign {
     struct Request {
-      string description;
-      uint value;
-      address payable recipient;
-      bool complete;
+        string description;
+        uint256 value;
+        address payable recipient;
+        bool complete;
     }
-    
+
     address public manager;
-    uint public minimumContribution;
+    uint256 public minimumContribution;
     address payable[] public approvers;
     Request[] public requests;
-    
-   modifier restricted() {
+
+    modifier restricted() {
         require(manager == msg.sender);
         _;
-   }    
+    }
 
-   constructor(uint minimum) {
+    constructor(uint256 minimum) {
         manager = msg.sender;
         minimumContribution = minimum;
-   }
-   
-   function contribute() public payable {
+    }
+
+    function contribute() public payable {
         require(msg.value >= minimumContribution);
         approvers.push(payable(msg.sender));
-   }
-   
-   function createRequest() public restricted {
-       
-   }
-   
-   function finalizeRequest() public restricted {
-       
-   }
-    
-    
+    }
+
+    function createRequest(
+        string memory description,
+        uint256 value,
+        address payable recipient
+    ) public restricted {
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false
+        });
+
+        requests.push(newRequest);
+    }
+
+    function approveRequest() public {}
+
+    function finalizeRequest() public restricted {}
 }
